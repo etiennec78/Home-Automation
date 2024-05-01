233a7753a77609bd6cfc5eeaba6d6b8f18ed9080
etiennec78 <etienne.coriou@free.fr>
2024-05-01 16:48:33 +0200

    Added Ble car device

---
+++ Automatic Gate/Extra/Ble car device/Ble car device.ino
@@ -0,0 +1,13 @@
+#include <BleKeyboard.h>
+#define USE_NIMBLE
+
+// Please modify bleKeyboard library manually to change the name of your ESP to the one you want
+
+BleKeyboard bleKeyboard;
+
+void setup() {
+  bleKeyboard.begin();
+}
+
+void loop() {
+}
