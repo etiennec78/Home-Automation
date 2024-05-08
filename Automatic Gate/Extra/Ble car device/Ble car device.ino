32a37276c22f500589a9746d617d52fd1eca4ffa
etiennec78 <59794011+etiennec78@users.noreply.github.com>
2024-05-08 19:09:35 +0200

    Added device name

--- Automatic Gate/Extra/Ble car device/Ble car device.ino
+++ Automatic Gate/Extra/Ble car device/Ble car device.ino
@@ -1,11 +1,10 @@
 #include <BleKeyboard.h>
 #define USE_NIMBLE
 
-// Please modify bleKeyboard library manually to change the name of your ESP to the one you want
-
 BleKeyboard bleKeyboard;
 
 void setup() {
+  bleKeyboard.setName('Vehicle');
   bleKeyboard.begin();
 }
 
